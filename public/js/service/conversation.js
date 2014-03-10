324623ab2a21bafcfb063d3194071b9a8011bc76
Bastian Krol <bastian.krol@web.de>
2014-03-10 13:03:00 +0100

    move conversation class to conversation service

--- public/js/service/conversation.js
+++ public/js/service/conversation.js
@@ -6,6 +6,19 @@
     .service('ConversationService',
       function(MessageService, socket, $interval) {
 
+    function Conversation(name, active) {
+      this.name = name;
+      this.active = !!active;
+    }
+
+    Conversation.prototype.getCssClasses = function() {
+      if (this.active) {
+        return ['list-group-item', 'active'];
+      } else {
+        return ['list-group-item'];
+      }
+    };
+
     var conversations = {};
     var currentConversation = {};
 
