06725941490c469cd490389e101f03776ae9f6cd
Bastian Krol <bastian.krol@web.de>
2014-03-10 13:17:23 +0100

    mark current user in user list

--- public/js/controller/user-list.js
+++ public/js/controller/user-list.js
@@ -4,10 +4,16 @@
   angular
     .module('plowderye')
     .controller('UserListCtrl',
-      function ($scope, socket) {
+      function ($scope, socket, UserService) {
+
+    $scope.isCurrentUser = function(user) {
+      return user === UserService.getUser();
+    };
+
     socket.on('fetch-users-result', function(users) {
-      $scope.users = users;
+      $scope.users = users ;
     });
+
   });
 
 })();
