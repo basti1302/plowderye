a88accfe0d059ed2f54821babfb5dda7dab9a59e
Bastian Krol <bastian.krol@web.de>
2014-04-12 16:06:49 +0200

    also watch main file

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 pushd `dirname $0`/.. > /dev/null
-nodemon --watch lib --watch node_modules plowderye.js --logging:console:enabled true --logging:console:level debug --logging:file:enabled false --logging:exceptions:console:enabled false --logging:exceptions:file:enabled false
+nodemon --watch plowderye.js --watch lib --watch node_modules plowderye.js --logging:console:enabled true --logging:console:level debug --logging:file:enabled false --logging:exceptions:console:enabled false --logging:exceptions:file:enabled false
 popd > /dev/null
